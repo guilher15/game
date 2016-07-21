bf42418481ba113f781405a9c2bfeaba7b4cce7d
guilher15 <guilhercasanova@hotmail.com>
2016-07-21 14:44:43 -0700

    Add files via upload

---
+++ js/CLang.js
@@ -0,0 +1,18 @@
+TEXT_GAMEOVER = "GAME OVER";
+TEXT_SCORE = "SCORE";
+TEXT_LEVEL = "LEVEL";
+TEXT_PT = "PT";
+TEXT_HELP = "Guess the word that links the 4 pics as fast as possible!If you use a help, yourscore will decrease!";
+TEXT_HELP2 = "Add a random\nletters to the\nsolution. This help\nwill cost you ";
+TEXT_HELP3 = "Remove some letters from the display. It will cost you ";
+TEXT_SELECT_LANG = "SELECT LANGUAGE";
+TEXT_CONGRATULATIONS = "Congratulations!";
+TEXT_CREDITS_DEVELOPED = "DEVELOPED BY";
+TEXT_LINK1 = "www.codethislab.com";
+
+TEXT_SHARE_IMAGE = "200x200.jpg";
+TEXT_SHARE_TITLE = "Congratulations!";
+TEXT_SHARE_MSG1 = "You collected <strong>";
+TEXT_SHARE_MSG2 = " points</strong>!<br><br>Share your score with your friends!";
+TEXT_SHARE_SHARE1 = "My score is ";
+TEXT_SHARE_SHARE2 = " points! Can you do better";
